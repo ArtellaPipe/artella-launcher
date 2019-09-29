166383d7cb53e86395fed8fb565b265642e731a4
Tomi Poveda <tpovedatd@gmail.com>
2019-09-29 20:02:42 +0200

    Moved file from artellalauncher repository

---
+++ source/artellapipe/launcher/__init__.py
@@ -0,0 +1,131 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Initialization module for artellapipe-launcher
+"""
+
+from __future__ import print_function, division, absolute_import
+
+__author__ = "Tomas Poveda"
+__license__ = "MIT"
+__maintainer__ = "Tomas Poveda"
+__email__ = "tpovedatd@gmail.com"
+
+import os
+import sys
+import inspect
+
+from tpPyUtils import importer, path as path_utils
+
+from artellapipe.utils import resource
+
+# =================================================================================
+
+current_project = None
+
+# =================================================================================
+
+
+class ArtellaLauncher(importer.Importer, object):
+    def __init__(self):
+        super(ArtellaLauncher, self).__init__(module_name='artellapipe.launcher')
+
+    def get_module_path(self):
+        """
+        Returns path where tpNameIt module is stored
+        :return: str
+        """
+
+        try:
+            mod_dir = os.path.dirname(inspect.getframeinfo(inspect.currentframe()).filename)
+        except Exception:
+            try:
+                mod_dir = os.path.dirname(__file__)
+            except Exception:
+                try:
+                    import tpDccLib
+                    mod_dir = tpDccLib.__path__[0]
+                except Exception:
+                    return None
+
+        return mod_dir
+
+
+def init(do_reload=False):
+    """
+    Initializes module
+    :param do_reload: bool, Whether to reload modules or not
+    """
+
+    packages_order = []
+
+    update_paths()
+    launcher_importer = importer.init_importer(importer_class=ArtellaLauncher, do_reload=False)
+    launcher_importer.import_packages(order=packages_order, only_packages=False)
+    if do_reload:
+        launcher_importer.reload_all()
+
+    create_logger_directory()
+
+    resources_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'resources')
+    resource.ResourceManager.instance().register_resource(resources_path)
+
+
+def update_paths():
+    """
+    Adds to sys.path necessary modules
+    :return:
+    """
+
+    dccs_path = get_dccs_path()
+    if dccs_path and os.path.isdir(dccs_path):
+        sys.path.append(dccs_path)
+
+
+def create_logger_directory():
+    """
+    Creates artellapipe logger directory
+    """
+
+    artellapipe_logger_dir = os.path.normpath(os.path.join(os.path.expanduser('~'), 'artellapipe', 'logs'))
+    if not os.path.isdir(artellapipe_logger_dir):
+        os.makedirs(artellapipe_logger_dir)
+
+
+def get_logging_config():
+    """
+    Returns logging configuration file path
+    :return: str
+    """
+
+    return os.path.normpath(os.path.join(os.path.dirname(__file__), '__logging__.ini'))
+
+
+def get_dccs_path():
+    """
+    Returns path where DCCs are located
+    :return: str
+    """
+
+    return path_utils.clean_path(os.path.join(os.path.dirname(__file__), 'dccs'))
+
+
+def get_launcher_config_path():
+    """
+    Returns path where default Artella launcher config is located
+    :return: str
+    """
+
+    from artellapipe.launcher.core import defines
+    return path_utils.clean_path(os.path.join(os.path.dirname(__file__), defines.ARTELLA_LAUNCHER_CONFIG_FILE_NAME))
+
+
+def get_updater_config_path():
+    """
+    Returns path where default Artella updater config is located
+    :return: str
+    """
+
+    from artellapipe.launcher.core import defines
+    return path_utils.clean_path(os.path.join(os.path.dirname(__file__), defines.ARTELLA_UPDATER_CONFIG_FILE_NAME))
