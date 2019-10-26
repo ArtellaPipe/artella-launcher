049e64f83ad6614f18ab67eb3fd29a4f6be01d4d
Tomi Poveda <tpovedatd@gmail.com>
2019-10-26 02:50:23 +0200

    Removed unnecessary DCC resources and fixed __init__.py file

--- artellapipe/launcher/core/__init__.py
+++ artellapipe/launcher/core/__init__.py
@@ -1,7 +1,5 @@
 order = [
     'artellapipe.launcher.core.defines',
-    'artellapipe.launcher.core.config',
-    'artellapipe.launcher.core.updater',
-    'artellapipe.launcher.core.dccselector',
-    'artellapipe.launcher.core.launcher',
+    'artellapipe.launcher.core.plugin',
+    'artellapipe.launcher.core.launcher'
 ]
